44962c90a3fa39ebbdcdb308d05a78399ed02216
Burak <Burak@DESKTOP-NPVCHFP>
2015-11-09 23:25:35 +0200

    first commit.

---
+++ Butterfly_Tv_Android_Player/gen/com/example/butterfly_tv_android_player/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.butterfly_tv_android_player;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
