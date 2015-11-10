78717e6311043f3afeccd39bbb265da57848ab07
Burak <Burak@169.254.143.118>
2015-11-10 22:32:51 +0200

    delete eclipse project.

--- Butterfly_Tv_Android_Player/gen/com/example/butterfly_tv_android_player/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.butterfly_tv_android_player;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
